f119bde6ca20da3ca0ccc0101658f11d5ce21145
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-04 17:45:03 +0200

    Complete 20

--- Ej18/ElectricCar.cs
+++ Ej18/ElectricCar.cs
@@ -8,7 +8,7 @@ namespace EjsTema3.Ej18
 {
     public class ElectricCar : Vehicle
     {
-        public ElectricCar(string brand, string batteryCapacity, string autonomy) : base(brand, batteryCapacity, autonomy) { }
+        public ElectricCar(string brand, double batteryCapacity, double autonomy) : base(brand, batteryCapacity, autonomy) { }
         public void TransportMode()
         {
             Console.WriteLine($"El vehicle es transoprta amb 'ElectricCar'\n Brand: {Brand}, Battery Capacity: {BatteryCapacity}, Autonomy: {Autonomy}");
