e262d18b944011f898217eaf1aa2316faba9aa15
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-04 13:28:25 +0200

    Complete 12

---
+++ Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsTema3
+{
+    public static class Calculadora
+    {
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Restar(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("No es pot dividir per zero.");
+                throw new DivideByZeroException("No es pot dividir per zero.");                
+            }
+            return a / b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,10 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Persona person1 = new Persona("Joan", 16);
-            person1.Saluda();
-            person1.Saluda("es");
-            person1.Saluda("en");
+            int suma = Calculadora.Sumar(3, 2);
+            int resta = Calculadora.Restar(5, 2);
+            int multiplicacio = Calculadora.Multiplicar(4, 3);
+            double divisio = Calculadora.Dividir(10, 2);
+
+            Console.WriteLine($"Suma: {suma}");
+            Console.WriteLine($"Resta: {resta}");
+            Console.WriteLine($"Multiplicació: {multiplicacio}");
+            Console.WriteLine($"Divisió: {divisio}");
         }
     }
 }
