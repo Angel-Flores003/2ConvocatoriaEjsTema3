838a7174c4e9a7b961ac112d4c0a9ec52384c0ca
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-04 14:09:00 +0200

    Complete 15

---
+++ Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsTema3
+{
+    public abstract class Animal
+    {
+        public string Nom { get; }
+        public Animal(string nom)
+        {
+            Nom = nom;
+        }
+        public abstract void So();
+    }
+}
