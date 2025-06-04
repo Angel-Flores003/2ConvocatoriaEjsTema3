7a8ee35aed1290df4392f60ff6cf98ea25fcb73d
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-04 17:31:21 +0200

    Complete 19

--- Ej18/ElectricCar.cs
+++ Ej18/ElectricCar.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 namespace EjsTema3.Ej18
 {
     public class ElectricCar : Vehicle
-    {        
+    {
         public ElectricCar(string brand, string batteryCapacity, string autonomy) : base(brand, batteryCapacity, autonomy) { }
         public void TransportMode()
         {
             Console.WriteLine($"El vehicle es transoprta amb 'ElectricCar'\n Brand: {Brand}, Battery Capacity: {BatteryCapacity}, Autonomy: {Autonomy}");
+        }
+        public override string ToString()
+        {
+            return $"ElectricCar: {Brand}, Battery Capacity: {BatteryCapacity}, Autonomy: {Autonomy}";
         }
     }
 }
